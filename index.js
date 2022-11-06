4946f096a77d7c74a05f77a061cbfa3e9225d0e6
nikitonya <nikitonya_s@mail.ru>
2022-11-06 14:06:08 +0400

    post request processing

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 8000
 const app = express()
 
+const urlencodedParser = express.urlencoded({extended: false});
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.get('/catalog', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'catalog.html'))
+    // res.json({ pervoePole: "123asd", vtoroePole: 345 })
+})
+
+app.post("/catalog", urlencodedParser, (req, res) =>{
+    if(!req.body) return res.sendStatus(400);
+    console.log(req.body);
+    res.send(`${req.body.userName} - ${req.body.userAge}`);
 })
 
 app.listen(PORT, ()=>{
