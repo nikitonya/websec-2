5a0ebda8ee42c84c044fdad3ace6602cab8a5764
nikitonya <nikitonya_s@mail.ru>
2022-11-07 13:36:54 +0400

    get and post requests are implemented

--- index.js
+++ index.js
@@ -5,23 +5,21 @@ const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 8000
 const app = express()
 
-const urlencodedParser = express.urlencoded({extended: false});
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+
+app.post("/user", jsonParser, function (request, response) {
+    console.log(request.body);
+    if(!request.body) return response.sendStatus(400);
+     
+    response.json(request.body); // отправляем пришедший ответ обратно
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-app.get('/catalog', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'catalog.html'))
-    // res.json({ pervoePole: "123asd", vtoroePole: 345 })
-})
-
-app.post("/catalog", urlencodedParser, (req, res) =>{
-    if(!req.body) return res.sendStatus(400);
-    console.log(req.body);
-    res.send(`${req.body.userName} - ${req.body.userAge}`);
-})
-
 app.listen(PORT, ()=>{
     console.log(`Server has been started on port ${PORT}...`)
 })
+
